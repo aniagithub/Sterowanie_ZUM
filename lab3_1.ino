e4840eae016fc95fd58c76c8c64de8c371993d69
aniagithub <34343888+aniagithub@users.noreply.github.com>
2019-01-28 20:54:28 +0100

    Lab3

---
+++ lab3_1.ino
@@ -0,0 +1,22 @@
+//Zadanie 1+2 - wysyłanie wartości liczbowej (pomiary z potencjometru)
+#include <ros.h>
+#include <std_msgs/Int16.h>
+int joypin_X = 0; 
+int value1= 0;
+ros::NodeHandle nh;
+std_msgs::Int16 str_msg;
+ros::Publisher data("data", &str_msg);
+
+void setup() {
+  nh.initNode();
+  nh.advertise(data);
+
+}
+
+void loop() {
+  value1 = analogRead(joypin_X);
+
+  str_msg.data = value1;
+  data.publish(&str_msg);
+  nh.spinOnce();
+}
